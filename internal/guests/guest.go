2ae4792653af20bc9c94fefa4151b338621ab49c
gui-laranjeira <guilr14@gmail.com>
2024-02-18 12:13:36 -0300

    feat: adding guest abstraction layer

--- internal/guests/guest.go
+++ internal/guests/guest.go
@@ -16,6 +16,34 @@ type Guest struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+type IGuestRepository interface {
+	CreateGuest(guest *Guest) (insertedID string, err error)
+	FindGuestById(id string) (*Guest, error)
+	FindGuestByEmail(email string) (*Guest, error)
+	UpdateGuest(guest *Guest) (rowsAffected int, err error)
+	DeleteGuest(id string) (rowsAffected int, err error)
+}
+
+type IGuestUseCase interface {
+	CreateGuest(CreateGuestInput) (*Guest, error)
+	FindGuestById(id string) (*Guest, error)
+	FindGuestByEmail(email string) (*Guest, error)
+	UpdateGuest(UpdateGuestInput) (*Guest, error)
+	DeleteGuest(id string) error
+}
+
+type CreateGuestInput struct {
+	UserId string `json:"user_id"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+}
+
+type UpdateGuestInput struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
 func NewGuest(userId string, name string, email string) (*Guest, error) {
 	userUUID, err := uuid.Parse(userId)
 	if err != nil {
